fa5ba2b711bf9dcb5648cb28f92630aeee5f99e2
Sedat Onat <sedatonat@outlook.com>
2022-11-11 19:13:27 +0300

    Update

---
+++ Project_99_Xc-Sandbox/View/Problem 1.swift
@@ -0,0 +1,34 @@
+////
+////  Kodeco_Core-Concepts.swift
+////  Project_99_Xc-Sandbox
+////
+////  Created by Sedat Onat on 10.11.2022.
+////
+//
+//import SwiftUI
+//
+//struct Kodeco_Core_Concepts: View {
+//    
+//    var petname: String
+//    var age: Int
+//    let catDef: String
+//    
+//    var body: some View {
+//
+//
+//        catDef = Text("\(petname) - \(age)"))
+//        
+//        }
+//    }
+//}
+//
+//
+//
+//
+//struct Kodeco_Core_Concepts_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Kodeco_Core_Concepts(catSef)
+//
+//    }
+//}
+//
---
+++ Project_99_Xc-Sandbox/View/Test 2.swift
@@ -0,0 +1,25 @@
+//
+//  Test 2.swift
+//  Project_99_Xc-Sandbox
+//
+//  Created by Sedat Onat on 11.11.2022.
+//
+
+import SwiftUI
+
+struct Test1: View {
+    var body: some View {
+        
+        
+        Text("hello")
+        
+        
+    }
+}
+
+
+struct Test1_previews: PreviewProvider {
+    static var previews: some View {
+        Test1()
+    }
+}
